52f4d1d8d94df264b458350ff369cf65819de73f
Daria Komarenko <dari.komarenko@gmail.com>
2024-04-20 15:52:55 +0300

    add services page and pictures

---
+++ src/app/services/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './style.module.scss'
+
+export default function Services() {
+  return (
+    <div className={style['services']}>
+        <h2>Чем я могу быть полезен</h2>
+    </div>
+  )
+}
