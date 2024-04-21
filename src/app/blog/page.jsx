97d4ad7e64b7f10e8270808695a258af48882696
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-04-21 21:58:03 +0300

    pagination was realized

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import style from './style.module.scss';
-import { blogData } from '../../constants/blog/blogData.js';
-import ArticleBlog from '@/components/ArticleBlog/ArticleBlog';
-import PaginationControls from '@/components/PaginationControls/PaginationControls';
+import BlogPage from '@/components/BlogPage/BlogPage';
 
-export default function Blog({ searchParams }) {
-  const page = searchParams['page'] ?? '1';
-  const per_page = searchParams['per_page'] ?? '4';
-  const start = (+page - 1) * +per_page;
-  const end = start + +per_page;
-  const entries = blogData.slice(start, end);
-
+export default function Blog() {
   return (
     <main className={style['blog']}>
       <h1 className={style['heading']}>Авторский блог</h1>
-      {entries.map((item) => {
-        return <ArticleBlog {...item} key={item.id} />;
-      })}
-      <PaginationControls
-        hasNextPage={end < blogData.length}
-        hasPrevPage={start > 0}
-      />
+      <BlogPage />
     </main>
   );
 }
---
+++ src/components/BlogPage/BlogPage.jsx
@@ -0,0 +1,67 @@
+'use client';
+import ArticleBlog from '../ArticleBlog/ArticleBlog';
+import Image from 'next/image';
+import arrowLeft from '../../../public/images/Blog/arrow-left.svg';
+import arrowRight from '../../../public/images/Blog/arrow-right.svg';
+import style from './style.module.scss';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { blogData } from '../../constants/blog/blogData.js';
+
+const paginate = (items, pageNumber, pageSize) => {
+  const startIndex = (pageNumber - 1) * pageSize;
+  return items.slice(startIndex, startIndex + pageSize);
+};
+
+export default function BlogPage() {
+  const items = blogData;
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 3;
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const [paginatedPosts, setPaginatedPosts] = useState(
+    paginate(items, currentPage, pageSize)
+  );
+  const pagesCount = Math.ceil(items.length / pageSize);
+
+  useEffect(() => {
+    setPaginatedPosts(paginate(items, currentPage, pageSize));
+    console.log(paginatedPosts);
+  }, [currentPage]);
+
+  return (
+    <>
+      {paginatedPosts.map((item) => {
+        return <ArticleBlog {...item} key={item.id} />;
+      })}
+      {pagesCount === 1 ? null : (
+        <span className={style['pagination__container']}>
+          <button
+            className={style['pagination__button']}
+            onClick={() => {
+              handlePageChange(currentPage - 1);
+            }}
+            disabled={currentPage - 1 <= 0}
+          >
+            <Image alt='arrow-left' src={arrowLeft} width={50} height={50} />
+          </button>
+          <span className={style['pagination__text']}>
+            {currentPage}/{pagesCount}
+          </span>
+
+          <button
+            className={style['pagination__button']}
+            onClick={() => {
+              handlePageChange(currentPage + 1);
+            }}
+            disabled={currentPage + 1 > pagesCount}
+          >
+            <Image alt='arrow-rigth' src={arrowRight} width={50} height={50} />
+          </button>
+        </span>
+      )}
+    </>
+  );
+}
