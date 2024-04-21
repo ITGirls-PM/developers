ca91833b7bf9937e78f3a18212950a24e0bc78ef
Lira11io <Lira11pm@mail.ru>
2024-04-21 16:14:40 +0300

    first

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Header from '../components/Header'
-
+import MainList from '../components/MainList'
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header />
+      <MainList />
     </main>
-  );
+  )
 }
---
+++ src/components/MainList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './style.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+import headerLogo from '../../../public/images/Header/header-logo.webp'
+import telegramLogo from '../../../public/images/Header/telegram-icon.svg'
+import linkedInLogo from '../../../public/images/Header/linkedin-icon.svg'
+import facebookLogo from '../../../public/images/Header/facebook-icon.svg'
+import setkaLogo from '../../../public/images/Header/setka-icon.svg'
+
+export default function MainList() {
+  return <div className={styles['mainblock']}>главная</div>
+}
