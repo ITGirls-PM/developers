2f0faa0dab9399b53c79fbf4fc642931df7a944e
Daria Komarenko <dari.komarenko@gmail.com>
2024-04-21 14:31:47 +0300

    add global css, add form validation

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -8,13 +8,39 @@ export default function Services() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
   
     const handleSubmit = (e) => {
       e.preventDefault();
-      console.log('Ваше имя:', name);
-      console.log('Электронный адрес:', email);
-      console.log('Ваше обращение:', message);
-    };
+      if (validateName(name) && validateEmail(email)) {
+          console.log('Ваше имя:', name);
+          console.log('Электронный адрес:', email);
+          console.log('Ваше обращение:', message);
+      }
+  };
+
+    const validateName = (value) => {
+      const regex = /^[a-zA-Zа-яА-Я\s]*$/;
+      if (!regex.test(value)) {
+          setNameError('Пожалуйста, введите только буквы и пробелы');
+          return false;
+      } else {
+          setNameError('');
+          return true;
+      }
+  };
+
+  const validateEmail = (value) => {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!regex.test(value)) {
+          setEmailError('Введите корректный email');
+          return false;
+      } else {
+          setEmailError('');
+          return true;
+      }
+  };
   return (
     <div className={style['services']}>
         <h2 className={style['services__heading']}>Чем я могу быть полезен</h2>
@@ -27,20 +53,24 @@ export default function Services() {
           type="text"
           id="name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {setName(e.target.value);
+          validateName(e.target.value)}}
           placeholder="Ваше имя"
         />
       </div>
+      {nameError && <span style={{ fontSize: 12, color: 'green' }}>{nameError}</span>}
       <div>
         <label htmlFor="email"></label>
         <input
           type="email"
           id="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {setEmail(e.target.value);
+          validateEmail(e.target.value)}}
           placeholder="Электронный адрес"
         />
       </div>
+      {emailError && <span>{emailError}</span>}
       <div>
         <label htmlFor="message"></label>
         <textarea
