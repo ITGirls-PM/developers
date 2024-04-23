d824ec5e44b8cc55d35e004f0f6ba2582cb02ae4
Daria Komarenko <dari.komarenko@gmail.com>
2024-04-23 10:37:09 +0300

    change favicon root and site name

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,16 @@
 import './globals.scss'
 import Header from '@/components/Header'
 
+
 export const metadata = {
-  title: 'ExecutiveMentorWebpage',
+  title: 'George Lambert',
   description: 'Личная страница Георга',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='ru' style={{ scrollBehavior: 'smooth' }} className='html'>
-      <link rel='icon' href='/favicon.ico' sizes='any' />
+      <link rel="icon" href="/images/favicon.ico" sizes="any" />
       <body className='body'>
         <Header />
         {children}
