77cb0586b6ffc9c147ec932aecacb7c67adb05fa
Daria Komarenko <dari.komarenko@gmail.com>
2024-04-24 14:44:32 +0300

    edit privacy-policy text

--- src/components/UpButton/index.jsx
+++ src/components/UpButton/index.jsx
@@ -8,10 +8,11 @@ import style from './style.module.scss';
 export default function UpButton(inView, showButton) {
 	return (
 		<Link
+		href=''
 			className={`${style['up-btn']} ${inView && showButton ? style['up-btn_inview'] : ''}`}
 			aria-label="Up"
 		>
-			<Image src={UpButtonIcon} alt='GoUp'/>
+			<Image src={UpButtonIcon} width={20} height={20} alt='GoUp'/>
 		</Link>
 	);
 }
