5beb80f38e55381429a1583ad33cabf7441773e1
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-04-24 20:47:33 +0300

    commit

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,6 +1,5 @@
 'use client';
 import Link from 'next/link';
-
 import style from './style.module.scss';
 import { useTranslation } from 'next-i18next';
 export default function NotFound() {
@@ -9,8 +8,8 @@ export default function NotFound() {
     <div className={style['not-found']}>
       <h1 className={style['not-found__name']}>404</h1>
       <p className={style['not-found__text']}>{t('not-found-title')}</p>
-      <button type='button' className={style['not-found__btn']}>
-        <Link href='/' className={style['not-found__link']}>
+      <button type="button" className={style['not-found__btn']}>
+        <Link href="/" className={style['not-found__link']}>
           {t('not-found-button')}
         </Link>
       </button>
